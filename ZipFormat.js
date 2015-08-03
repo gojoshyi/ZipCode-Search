ea4945628a428d16d393ade4d430929820e50a34
gojoshyi <gojoshyi@gmail.com>
2015-08-03 01:42:17 -0400

    Initial
    Initial Commit

---
+++ ZipFormat.js
@@ -0,0 +1,109 @@
+﻿function Locations(){
+    this.NY = {
+        Flushing: {
+            name: "Flushing",
+            href: "/Flushing",
+            addr1: "25-44 Francis Lewis Blvd.",
+            addr2: "Flushing NY 11358",
+            phone: "(718) 353-1533",
+            zip: "11358",
+            lookUp: new ZipLookUp().Flushing.zip_codes
+        },
+
+        Ossining: {
+            name: "Ossining",
+            href: "/Ossining",
+            addr1: "109 Croton Ave.",
+            addr2: "Ossining NY 10562",
+            phone: " (914) 588-8618",
+            zip: "10562",
+            lookUp: new ZipLookUp().Ossining.zip_codes
+        },
+
+        Eastchester: {
+            name: "Eastchester",
+            href: "/Eastchester",
+            addr1: "360 White plains Rd.",
+            addr2: "Eastchester NY 10709",
+            phone: "(914) 779-8000",
+            zip: "10709",
+            lookUp: new ZipLookUp().Eastchester.zip_codes
+        },
+
+        Woodside: {
+            name: "Woodside",
+            href: "/Woodside",
+            addr1: "69-01 Northern Blvd.",
+            addr2: "Woodside NY 11377",
+            phone: "(718) 803-1700",
+            zip: "11377",
+            lookUp: new ZipLookUp().Woodside.zip_codes
+        },
+
+        JacksonHeights: {
+            name: "Jackson Heights",
+            href: "/JacksonHeights",
+            addr1: "84-02 Northern Blvd.",
+            addr2: "Jackson Heights NY 11372",
+            phone: "(718) 507-6500",
+            zip: "11372",
+            lookUp: new ZipLookUp().JacksonHeights.zip_codes
+        }
+    }//NY Locations
+
+    this.NJ = {
+        FortLee: {
+            name: "Fort Lee",
+            href: "/FortLee",
+            addr1: "1211 Anderson Ave",
+            addr2: "Fort Lee NJ 07024",
+            phone: "(201) 224-7800",
+            zip: "07024",
+            lookUp: new ZipLookUp().FortLee.zip_codes
+        },
+
+        Hackensack: {
+            name: "Hackensack",
+            href: "/Hackensack",
+            addr1: "380 W. Pleasantview Ave.",
+            addr2: "Hackensack NJ 07601",
+            phone: "(201) 342-2400",
+            zip: "07601",
+            lookUp: new ZipLookUp().Hackensack.zip_codes
+        },
+
+        WestNY: {
+            name: "West New York",
+            href: "/WestNY",
+            addr1: "4915 Bergenline Ave.",
+            addr2: "West New York NJ 07093",
+            phone: "(201) 223-0200",
+            zip: "07093",
+            lookUp: new ZipLookUp().WestNY.zip_codes
+        },
+
+        NorthBergen: {
+            name: "North Bergen",
+            href: "/NorthBergen",
+            addr1: "7705 Bergenline Avenue",
+            addr2: "North Bergen NJ 07047",
+            phone: "(201) 868-5425",
+            zip: "07047",
+            lookUp: new ZipLookUp().NorthBergen.zip_codes
+        },
+
+        Nutley: {
+            name: "Nutley",
+            href: "/Nutley",
+            addr1: "242 Franklin Ave",
+            addr2: "Nutley NJ 07110",
+            phone: "(973) 542-1200",
+            zip: "07110",
+            lookUp: new ZipLookUp().Nutley.zip_codes
+        }
+    }//NJ Locations
+
+    return this;
+};
+
+
---
+++ ZipSearch.js
@@ -0,0 +1,100 @@
+﻿$(document).ready(function () {
+    var defaultLocations = function () {
+        $("#ny-locations").empty();
+        $("#nj-locations").empty();
+        $("#zip").val("");
+
+        var loc = new Locations();
+
+        for (var i in loc.NY) {
+            $("#ny-locations").append(GetAddrString(loc.NY[i]));
+        };
+
+        for (var j in loc.NJ) {
+            $("#nj-locations").append(GetAddrString(loc.NJ[j]));
+        };
+    };
+    defaultLocations();
+
+    $("#zip").keydown(function (e) {
+
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+            // Allow: Ctrl+A, Command+A
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Allow: home, end, left, right, down, up
+            (e.keyCode >= 35 && e.keyCode <= 40)) {
+            // let it happen, don't do anything
+            return;
+        }
+        if (($("#zip").val().length > 4)) {
+            e.preventDefault();
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+
+    $("#resetLocations").on("click", function () {
+        defaultLocations();
+    });
+
+    $("#zipSearch").on("click", function () {
+
+        $("#ny-locations").empty();
+        $("#nj-locations").empty();
+
+        // Build url
+        var zipcode = $("#zip").val();
+        var distance = $("#distance").val();
+
+        var searchLoc = new Locations();
+
+        for (var i in searchLoc.NY) {
+            for (var j in searchLoc.NY[i].lookUp) {
+                if (searchLoc.NY[i].lookUp[j].zip_code == zipcode) {
+                    if (searchLoc.NY[i].lookUp[j].distance <= distance) {
+                        $("#ny-locations").append(GetAddrStringWithDistance(searchLoc.NY[i], searchLoc.NY[i].lookUp[j].distance));
+                        break;
+                    }
+                }
+            }
+        };
+
+        for (var a in searchLoc.NJ) {
+            for (var b in searchLoc.NJ[a].lookUp) {
+                if (searchLoc.NJ[a].lookUp[b].zip_code == zipcode) {
+                    if (searchLoc.NJ[a].lookUp[b].distance <= distance) {
+                        $("#nj-locations").append(GetAddrStringWithDistance(searchLoc.NJ[a], searchLoc.NJ[a].lookUp[b].distance));
+                        break;
+                    }
+                }
+            }
+        };
+
+    });
+});
+
+function GetAddrString(location) {
+    var addr = 
+    "<div class='col-md-3'><a href='" + location.href + 
+    "'><h4><i class='fa fa-map-marker'></i>" + location.name + 
+    "</h4></a><p>" + location.addr1 +
+    "<br />" + location.addr2 +
+    "<br /><span class='fa fa-phone'></span> " + location.phone + "</div>";
+
+    return addr;
+};//Print Addr Function
+
+function GetAddrStringWithDistance(location,distance) {
+    var addr =
+    "<div class='col-md-3'><a href='" + location.href +
+    "'><h4><i class='fa fa-map-marker'></i>" + location.name +
+    "</h4></a><p>" + location.addr1 +
+    "<br />" + location.addr2 +
+    "<br /><span class='fa fa-phone'></span> " + location.phone +
+    "<br/><span class='fa fa-car'> "+ (Math.round( distance * 10) / 10) +" miles away</span></div>";
+
+    return addr;
+};//Print Addr With Distance Function
